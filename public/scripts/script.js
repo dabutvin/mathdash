72f1f24504abcc55b73031bab696b3de19772fac
dabutvin <dabutvin@microsoft.com>
2015-03-17 22:46:57 -0700

    slick slider
    Selfhost foundation

---
+++ public/scripts/script.js
@@ -0,0 +1,5 @@
+$(document).foundation();
+
+$(window).load(function() {
+    $(".hero-panel").orbit();
+});
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,18 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express', cache: false });
+  res.render(
+    'index',
+    {
+        title: 'Mathdash',
+        slick: {
+            'slidesToShow': 3,
+            'slidesToScroll': 2,
+            'arrows': false,
+            'mobileFirst': true
+        },
+        cache: false
+    });
 });
 
 module.exports = router;
