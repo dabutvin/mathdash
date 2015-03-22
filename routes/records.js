7f4c5091df88a43d63bc3cf187059aabf659a247
dabutvin <dabutvin@microsoft.com>
2015-03-22 11:24:45 -0700

    records

--- routes/records.js
+++ routes/records.js
@@ -3,21 +3,21 @@ var router = express.Router();
 var azure = require('azure-storage');
 var entGen = azure.TableUtilities.entityGenerator;
 
-var storageAccountName = "mathdashscorestest";
-var storageAccountKey = "somehash";
-
-var retryOperations = new azure.ExponentialRetryPolicyFilter();
-var tableSvc = azure.createTableService(storageAccountName, storageAccountKey).withFilter(retryOperations);
+var storageAccountName = "dansstore";
+var storageAccountKey = "P5+vMg9qq5ryq04d0OfOny2N///DXz9Mopmo52hlswCgZeeDSW1SNFSwX9AAYDCpzE6bEN9jjrBnRtuo2SD5fg==";
 
 var globalTable = 'records';
 var globalPartition = 'partition1';
 
 /* GET records listing. */
 router.get('/', function(req, res) {
+    var retryOperations = new azure.ExponentialRetryPolicyFilter();
+    var tableSvc = azure.createTableService(storageAccountName, storageAccountKey).withFilter(retryOperations);
+
     tableSvc.createTableIfNotExists(globalTable, function(error, result, response) {
         if(!error) {
             var query = azure.TableQuery()
-            .top(5);
+            // .top(5);
             //.where('PartitionKey eq ?', 'part2');
 
             tableSvc.queryEntities(globalTable, query, null, function(error, result, response) {
@@ -43,10 +43,13 @@ router.post('/', function(req, res) {
         difficulty: entGen.String(req.body.difficulty)
     };
 
+    var retryOperations = new azure.ExponentialRetryPolicyFilter();
+    var tableSvc = azure.createTableService(storageAccountName, storageAccountKey).withFilter(retryOperations);
+
     tableSvc.createTableIfNotExists(globalTable, function(error, result, response) {
         if(!error) {
             // Try to retrieve the entitiy
-            tableSvc.retrieveEntity(globalTable, globalPartition, newRecord.RowKey, function(error, result, response) {
+            tableSvc.retrieveEntity(globalTable, globalPartition, req.body.deviceId, function(error, result, response) {
                 if(!error){
                     // result contains the entity
                     if (result.entity) {
@@ -73,7 +76,13 @@ router.post('/', function(req, res) {
                         });
                     }
                 } else {
-                    res.status(500).send("Error retrieving entity");
+                    tableSvc.insertEntity(globalTable, newRecord, function(error, result, response) {
+                        if (!error) {
+                            res.status(201).send("First record inserted");
+                        } else {
+                            res.status(500).send("Error inserting first record");
+                        }
+                    });
                 }
             });
         } else {
