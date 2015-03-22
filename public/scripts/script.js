14a4d57e761f7d221b57bc8bad7fe01d34428397
dabutvin <dabutvin@microsoft.com>
2015-03-22 14:25:06 -0700

    cleanup

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,1 +1,13 @@
-$(document).foundation();
+var CommentBox = React.createClass({
+  render: function() {
+    return (
+      <div className="commentBox">
+        Hello, world! I am a CommentBox.
+      </div>
+    );
+  }
+});
+React.render(
+  <CommentBox />,
+  document.getElementById('content')
+);
